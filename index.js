27466376a484013ca60ec3651283411ecfb59f95
Rahul Kumar <developerrahul977@gmail.com>
2025-10-04 02:11:02 +0530

    recover

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function index() {
+  return (
+   <>
+    <div>index i am index file</div>
+    <div> dont delete it very dabger</div>
+    </>
+  )
+   
+}
